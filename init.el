eca6c707ed59417b1b5d7e6519475b65d08761d0
Craig Andera <candera@wangdera.com>
2011-06-19 16:42:31 -0400

    Changing background to black

--- init.el
+++ init.el
@@ -771,7 +771,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- `(default ((t ,(append '(:background "grey10" :foreground "white")
+ `(default ((t ,(append '(:background "black" :foreground "white")
 			(if (eq system-type 'windows-nt)
 			    '(:foundry "outline" 
 			      :family "Courier New"
