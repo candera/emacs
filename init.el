f994dbce136806e54111d92dd0772cb34cd123ac
Craig Andera <candera@wangdera.com>
2011-07-20 22:05:14 -0400

    Autoload sparql-mode.

--- init.el
+++ init.el
@@ -794,8 +794,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (add-to-list 'load-path "~/.emacs.d/custom/sparql-mode")
-(require 'sparql-mode)
-
+(autoload 'sparql-mode "sparql-mode.el"
+     "Major mode for editing SPARQL files" t)
 (add-to-list 'auto-mode-alist '("\\.sparql$" . sparql-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
