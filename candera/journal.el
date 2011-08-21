404c6be40ae0fbd7a20725b76202a4983b770dae
Craig Andera <candera@wangdera.com>
2011-08-21 07:55:55 -0400

    Fix up whitespace.

--- candera/journal.el
+++ candera/journal.el
@@ -13,32 +13,32 @@
              (list (read-directory-name "Base directory: "))
            *journal-roots*)))
     (let*
-	((n-days-ago (if (null days-ago) 1 days-ago))
-	 (logfile-date (time-n-days-ago n-days-ago))
-	 (logfile-directory (available-logfile-directory *journal-roots*))
-	 (new-logfile-directory (format-time-string (concat logfile-directory "%Y/%m-%b") logfile-date))
-	 (new-logfile-filename 
-	  (format-time-string 
-	   (concat new-logfile-directory "/%Y%m%d.txt") logfile-date)))
-      (progn 
-	(make-directory new-logfile-directory 't)
-	(let ((existing? 
-	       (or (find-buffer-visiting new-logfile-filename)
-		   (file-exists-p new-logfile-filename))))
-	  (find-file new-logfile-filename)
-	  (unless existing?
-	    (insert (concat (format-time-string "%A, %B " logfile-date)
-			    (day-of-month-ordinal 
-			     (string-to-number 
-			      (format-time-string "%e" logfile-date)))
-			    (format-time-string ", %Y." logfile-date)))
-	    (newline)
-	    (newline)
-	    (newline)
-	    (previous-line)
-	    (flyspell-mode 1)
-	    (message (concat "Opened " new-logfile-filename))))))))
-  
+        ((n-days-ago (if (null days-ago) 1 days-ago))
+         (logfile-date (time-n-days-ago n-days-ago))
+         (logfile-directory (available-logfile-directory *journal-roots*))
+         (new-logfile-directory (format-time-string (concat logfile-directory "%Y/%m-%b") logfile-date))
+         (new-logfile-filename
+          (format-time-string
+           (concat new-logfile-directory "/%Y%m%d.txt") logfile-date)))
+      (progn
+        (make-directory new-logfile-directory 't)
+        (let ((existing?
+               (or (find-buffer-visiting new-logfile-filename)
+                   (file-exists-p new-logfile-filename))))
+          (find-file new-logfile-filename)
+          (unless existing?
+            (insert (concat (format-time-string "%A, %B " logfile-date)
+                            (day-of-month-ordinal
+                             (string-to-number
+                              (format-time-string "%e" logfile-date)))
+                            (format-time-string ", %Y." logfile-date)))
+            (newline)
+            (newline)
+            (newline)
+            (previous-line)
+            (flyspell-mode 1)
+            (message (concat "Opened " new-logfile-filename))))))))
+
 ;; (defun days-ago (n)
 ;;   "Returns a value similar to current-time, but for n days ago"
 ;;   (interactive)
@@ -62,12 +62,12 @@
 (defun day-of-month-ordinal (n)
    "Returns ordinal for range 1-31 (1st, 2nd, 3rd, etc.)"
    ()
-   (if (= n 1) "1st" 
+   (if (= n 1) "1st"
      (if (= n 2) "2nd"
        (if (= n 3) "3rd"
-	 (if (= n 21) "21st"
-	   (if (= n 22) "22nd"
-	     (if (= n 23) "23rd"
-	       (if (= n 31) "31st" 
-		 (if (and (> n 0) (< n 31)) (concat (number-to-string n) "th") nil)))))))))
+         (if (= n 21) "21st"
+           (if (= n 22) "22nd"
+             (if (= n 23) "23rd"
+               (if (= n 31) "31st"
+                 (if (and (> n 0) (< n 31)) (concat (number-to-string n) "th") nil)))))))))
 
