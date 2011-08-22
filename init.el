0bde3712d4d858d5f1795bb739c05ab22e36550c
Craig Andera <candera@wangdera.com>
2011-08-22 08:40:23 -0400

    Turn on paredit in emacs-lisp-mode

--- init.el
+++ init.el
@@ -375,6 +375,7 @@
   "Minor mode for pseudo-structurally editing Lisp code."
   t)
 (add-hook 'lisp-mode-hook '(lambda () (paredit-mode +1)))
+(add-hook 'emacs-lisp-mode-hook '(lambda () (paredit-mode +1)))
 
 (show-paren-mode t)
 
