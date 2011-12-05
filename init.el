b4d130f2ebfb89b73a4d160874077f16c4660f91
Craig Andera <candera@wangdera.com>
2011-12-05 13:02:49 -0500

    Allow forms to be sent to shell rather than inferior-lisp

--- init.el
+++ init.el
@@ -529,6 +529,51 @@
 ;; (add-hook 'slime-mode-hook 'set-up-slime-ac)
 ;; (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Allow input to be sent to somewhere other than inferior-lisp
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; This is a total hack: we're hardcoding the name of the shell buffer
+(defun shell-send-input (input)
+  "Send INPUT into the *shell* buffer and leave it visible."
+  (save-excursion
+    (switch-to-buffer-other-window "*shell*")
+    (goto-char (point-max))
+    (insert input)
+    (comint-send-input)))
+
+(defun defun-at-point ()
+  "Return the text of the defun at point."
+  (apply #'buffer-substring-no-properties
+         (region-for-defun-at-point)))
+
+(defun region-for-defun-at-point ()
+  "Return the start and end position of defun at point."
+  (save-excursion
+    (save-match-data
+      (end-of-defun)
+      (let ((end (point)))
+        (beginning-of-defun)
+        (list (point) end)))))
+
+(defun expression-preceding-point ()
+  "Return the expression preceding point as a string."
+  (buffer-substring-no-properties
+   (save-excursion (backward-sexp) (point))
+   (point)))
+
+(defun shell-eval-last-expression ()
+  "Send the expression preceding point to the *shell* buffer."
+  (interactive)
+  (shell-send-input (expression-preceding-point)))
+
+(defun shell-eval-defun ()
+  "Send the current toplevel expression to the *shell* buffer."
+  (interactive)
+  (shell-send-input (defun-at-point)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; This section sets up clojure-mode
@@ -541,6 +586,10 @@
 (autoload 'clojure-mode "clojure-mode" "A major mode for Clojure" t)
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
 
+(add-hook 'clojure-mode-hook
+          '(lambda ()
+             (define-key clojure-mode-map (kbd "C-c e") 'shell-eval-last-expression)
+             (define-key clojure-mode-map (kbd "C-c x") 'shell-eval-defun)))
 
 ;; Turn on paredit for clojure files
 ;(require 'clojure-paredit)
