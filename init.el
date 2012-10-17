042ce58b3f3f3b6a38e7a14f64fda9b4ba2a5b88
Craig Andera <candera@wangdera.com>
2012-10-17 13:25:21 -0400

    Include .java files in find-file-in-project

--- init.el
+++ init.el
@@ -973,7 +973,7 @@ if the major mode is one of 'delete-trailing-whitespace-modes'"
 
 (require 'find-file-in-project)
 (global-set-key (kbd "C-x M-f") 'find-file-in-project)
-(setq ffip-patterns (append ffip-patterns '("*.cljs" "*.scss" ".css")))
+(setq ffip-patterns (append ffip-patterns '("*.cljs" "*.scss" ".css" "*.java")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
