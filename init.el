d995a81fe57accd175b3506fdcd3fca651534b21
Craig Andera <candera@wangdera.com>
2012-12-03 14:48:54 -0500

    Add yet more files to the ffip filter.

--- init.el
+++ init.el
@@ -975,7 +975,7 @@ if the major mode is one of 'delete-trailing-whitespace-modes'"
 
 (require 'find-file-in-project)
 (global-set-key (kbd "C-x M-f") 'find-file-in-project)
-(setq ffip-patterns (append ffip-patterns '("*.cljs" "*.scss" ".css" "*.java")))
+(setq ffip-patterns (append ffip-patterns '("*.cljs" "*.scss" ".css" "*.java" "*.dtm" "*.edn")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
