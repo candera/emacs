5f0af216d50544959542e5a66b164a7b5f4b2de5
Craig Andera <candera@wangdera.com>
2016-03-21 21:53:22 -0700

    Work around stupid bug in string-match-p.

--- candera/journal.el
+++ candera/journal.el
@@ -34,7 +34,7 @@
                             (format-time-string ", %Y." logfile-date)))
             ;; Auto save over SSH is a PITA. This will still auto-save
             ;; on idle.
-            (when (zerop (string-match-p "/ssh:" new-logfile-filename))
+            (when (numberp (string-match "/ssh:" new-logfile-filename))
               (set-variable 'auto-save-interval 0))
             (newline)
             (newline)
